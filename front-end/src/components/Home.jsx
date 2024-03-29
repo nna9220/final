f3dbf09b6b31651131aab9d54f372ab4d484a0a7
Nguyen <nna9220@gmail.com>
2024-03-29 21:20:42 +0700

    fix home

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import axios from 'axios';
 
 function Home() {
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`http://localhost:5000/api/home`);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+    }, []);
+
     return (
         <div class="hero">
             <div class="card text-bg-white">
