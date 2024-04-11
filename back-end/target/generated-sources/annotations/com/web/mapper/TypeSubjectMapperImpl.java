75b06095b194edc61e135f1afca74caedab97dc1
Nguyen <nna9220@gmail.com>
2024-04-11 22:22:31 +0700

    fix create KLTN

--- back-end/target/generated-sources/annotations/com/web/mapper/TypeSubjectMapperImpl.java
+++ back-end/target/generated-sources/annotations/com/web/mapper/TypeSubjectMapperImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-04-11T16:49:24+0700",
+    date = "2024-04-11T22:21:15+0700",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.8 (Oracle Corporation)"
 )
 @Component
