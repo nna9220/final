dcee5381e5e3e405aaa8b84b1821e3ea919d4140
ThuyyyTranggg <thuytrangh4chuyen@gmail.com>
2024-04-11 16:48:28 +0700

    fix title

--- front-end/src/Pages/Head/HomeHead.jsx
+++ front-end/src/Pages/Head/HomeHead.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import SidebarHead from '../../components/Sidebar/SidebarHead'
 import Navbar from '../../components/Navbar/Navbar'
 import './HomeHead.scss'
-
+import { useEffect } from 'react'
 function HomeHead() {
+  useEffect(() => {
+    document.title = "Trang chủ Trưởng bộ môn";
+  }, []);
   return (
     <div className='homeHead'>
       <SidebarHead/>
