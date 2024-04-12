82fa3b69c25e750685d44747a5fcf8cfafc62a15
Nguyen <nna9220@gmail.com>
2024-04-12 23:45:55 +0700

    urrl edit period

--- front-end/src/components/dataTable/DataTableRegistrationPeroidLec.jsx
+++ front-end/src/components/dataTable/DataTableRegistrationPeroidLec.jsx
@@ -44,7 +44,7 @@ function DataTableRegistrationPeroidLec() {
 
         if (tokenSt && selectedPeriodId) {
             console.log("Data: ", updatedEndValue,updatedStartValue);
-            axios.put(`/api/admin/PeriodLecturer/edit/${selectedPeriodId}`,{
+            axiosInstance.put(`/admin/PeriodLecturer/edit/${selectedPeriodId}`,{
                 params:{
                 periodId: selectedPeriodId,
                 start: updatedStartValue,
