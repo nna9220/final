937b4bebb8ab754549bb0e8ddb4838f002b99fd5
ThuyyyTranggg <thuytrangh4chuyen@gmail.com>
2024-04-16 10:16:47 +0700

    add list KLTN for lecturer

--- front-end/src/components/KanbanOfLecturer/ManagermentTask.jsx
+++ front-end/src/components/KanbanOfLecturer/ManagermentTask.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Booard from './Booard'
 import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
 import DetailsIcon from '@mui/icons-material/Details';
+import TopicOutlinedIcon from '@mui/icons-material/TopicOutlined';
+import SummarizeOutlinedIcon from '@mui/icons-material/SummarizeOutlined';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { getTokenFromUrlAndSaveToStorage } from '../tokenutils';
@@ -9,6 +11,8 @@ import axiosInstance from '../../API/axios';
 
 function ManagermentTask() {
   const [topics, setTopics] = useState([]);
+  const [activeTLChuyenNganh, setActiveTLChuyenNganh] = useState(false);
+  const [activeKhoaLuan, setActiveKhoaLuan] = useState(false);
   const [showManagementTask, setShowManagementTask] = useState(false);
   const [selectedSubjectId, setSelectedSubjectId] = useState(null);
   const [selectedSubjectName, setSelectedSubjectName] = useState("");
@@ -16,26 +20,42 @@ function ManagermentTask() {
   const userToken = getTokenFromUrlAndSaveToStorage();
 
   useEffect(() => {
-    console.log("TokenTopic: " + userToken);
-    if (userToken) {
-      const tokenSt = sessionStorage.getItem(userToken);
-      if (!tokenSt) {
-        axiosInstance.get('/lecturer/subject', {
-          headers: {
-            'Authorization': `Bearer ${userToken}`,
-          },
-        })
-          .then(response => {
-            console.log("Topic: ", response.data);
-            setTopics(response.data.listSubject);
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      }
-    }
+    listTopic();
   }, [userToken]);
 
+  const listTopic = () => {
+    axiosInstance.get('/lecturer/subject', {
+      headers: {
+        'Authorization': `Bearer ${userToken}`,
+      }
+    })
+      .then(response => {
+        console.log("TopicTL: ", response.data);
+        setTopics(response.data.listSubject);
+        setActiveTLChuyenNganh(true);
+        setActiveKhoaLuan(false);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  const listSubjectGraduation = () => {
+    axiosInstance.get('/lecturer/subjectGraduation', {
+      headers: {
+        'Authorization': `Bearer ${userToken}`,
+      }
+    })
+      .then(response => {
+        console.log("TopicKL: ", response.data);
+        setTopics(response.data.listSubject);
+        setActiveKhoaLuan(true);
+        setActiveTLChuyenNganh(false);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
   const handleShowManagementTask = (subjectId, subjectName) => { // Thêm tham số để nhận tên đề tài
     setSelectedSubjectId(subjectId);
     setSelectedSubjectName(subjectName); // Lưu trữ tên đề tài được chọn
@@ -50,6 +70,14 @@ function ManagermentTask() {
 
   return (
     <div className='home-table'>
+      <div className='btn-type'>
+        <button className={`button-listDelete ${activeTLChuyenNganh ? 'active' : ''}`} onClick={listTopic}>
+          <TopicOutlinedIcon /> Tiểu luận chuyên ngành
+        </button>
+        <button className={`button-listDelete ${activeKhoaLuan ? 'active' : ''}`} onClick={listSubjectGraduation}>
+          <SummarizeOutlinedIcon /> Khóa luận tốt nghiệp
+        </button>
+      </div>
       {showBackButton && (
         <nav aria-label="breadcrumb">
           <ol className="breadcrumb">
@@ -70,6 +98,8 @@ function ManagermentTask() {
               <th scope="col">GVPB</th>
               <th scope="col">Sinh viên 1</th>
               <th scope="col">Sinh viên 2</th>
+              <th scope="col">Sinh viên 3</th>
+              <th scope='col'>Loại đề tài</th>
               <th scope="col">Yêu cầu</th>
               <th scope="col">Action</th>
             </tr>
@@ -83,6 +113,8 @@ function ManagermentTask() {
                 <td>{item.thesisAdvisorId?.person?.firstName + ' ' + item.thesisAdvisorId?.person?.lastName}</td>
                 <td>{item.student1 || ''}</td>
                 <td>{item.student2 || ''}</td>
+                <td>{item.student3 || ''}</td>
+                <td>{item.typeSubject.typeName || ''}</td>
                 <td>{item.requirement}</td>
                 <td>
                   <button
