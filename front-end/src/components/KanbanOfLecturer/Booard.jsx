ebaa781f1c87413b9f8f340eaf219c8c1963511b
ThuyyyTranggg <thuytrangh4chuyen@gmail.com>
2024-04-19 16:33:48 +0700

    add timeline for lecturer and header, fix UI list task for student

--- front-end/src/components/KanbanOfLecturer/Booard.jsx
+++ front-end/src/components/KanbanOfLecturer/Booard.jsx
@@ -5,41 +5,90 @@ import { DragDropContext } from 'react-beautiful-dnd';
 import Column from './Column';
 import './Styles.scss'
 import axiosInstance from '../../API/axios';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
+import TimeLineOfLecturer from '../Timeline/TimeLineOfLecturer';
+import DnsOutlinedIcon from '@mui/icons-material/DnsOutlined';
 
-const Booard=({subjectId})=>{
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        const userToken = getTokenFromUrlAndSaveToStorage();
-        const fetchData = async () => {
-          try {
-            const response = await axiosInstance.get(`/lecturer/subject/listTask/${subjectId}`, {
-              headers: {
-                'Authorization': `Bearer ${userToken}`,
-              },
-            });
-            setData(response.data.listTask);
-            console.log("DataTask:", response.data);
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
-        };
-    
-        fetchData();
-      }, [subjectId]);
+const Booard = ({ subjectId }) => {
+  const [data, setData] = useState([]);
+  const [newTask, setNewTask] = useState([]);
+  const [selectedSubjectId, setSelectedSubjectId] = useState(null);
+  const [showTimeLine, setShowTimeLine] = useState(false);
+  const [showListTask, setShowListTask] = useState(true);
+  const [formNewTask, setFormNewTask] = useState({
+    requirement: '',
+    timeStart: '',
+    timeEnd: '',
+    assignTo: '',
+  })
+
+  const handleChangeAdd = (e) => {
+    const { name, value } = e.target;
+    setFormNewTask(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
+  const handleSubjectId = () => {
+    setSelectedSubjectId(subjectId);
+  }
+  const toggleTimeline = () => {
+    setShowTimeLine(!showTimeLine);
+    setShowListTask(false);
+  }
+  const toggleListTask = () => {
+    setShowListTask(!showListTask);
+    setShowTimeLine(false);
+  }
+  useEffect(() => {
+    const userToken = getTokenFromUrlAndSaveToStorage();
+    const fetchData = async () => {
+      try {
+        const response = await axiosInstance.get(`/lecturer/subject/listTask/${subjectId}`, {
+          headers: {
+            'Authorization': `Bearer ${userToken}`,
+          },
+        });
+        setData(response.data.listTask);
+        console.log("DataTask:", response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [subjectId]);
 
   const onDragEnd = (result) => {
     // Code xử lý kéo và thả task tại đây
   };
   return (
     <div>
-        <DragDropContext onDragEnd={onDragEnd}>
-      <div className="kanban-board">
-        <Column className='column' title="Must Do" tasks={data.filter(task => task.status === 'MustDo')} droppableId="MustDo" />
-        <Column className='column' title="Doing" tasks={data.filter(task => task.status === 'Doing')} droppableId="Doing" />
-        <Column className='column' title="Closed" tasks={data.filter(task => task.status === 'Closed')} droppableId="Closed" />
-        {/* Các cột khác */}
+      <div>
+        <div className='list-button' style={{display:'flex', justifyContent:'flex-end'}}>
+          <div>
+            <button type="button" className='button-add-task' onClick={toggleListTask}>
+              <DnsOutlinedIcon /> List Task
+            </button>
+            <button type="button" className='button-add-task' onClick={toggleTimeline}>
+              <TimelineOutlinedIcon subjectId={subjectId} /> Time Line
+            </button>
+          </div>
+        </div>
       </div>
-    </DragDropContext>
+      {showListTask && (
+        <DragDropContext onDragEnd={onDragEnd}>
+          <div className="kanban-board">
+            <Column className='column' title="Must Do" tasks={data.filter(task => task.status === 'MustDo')} droppableId="MustDo" />
+            <Column className='column' title="Doing" tasks={data.filter(task => task.status === 'Doing')} droppableId="Doing" />
+            <Column className='column' title="Closed" tasks={data.filter(task => task.status === 'Closed')} droppableId="Closed" />
+            {/* Các cột khác */}
+          </div>
+        </DragDropContext>
+      )}
+      {showTimeLine && <TimeLineOfLecturer subjectId={subjectId} />}    
     </div>
   )
 }
