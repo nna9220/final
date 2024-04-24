009ad74e3d236fa81190704c9b6c49575c386729
ThuyyyTranggg <thuytrangh4chuyen@gmail.com>
2024-04-24 11:35:41 +0700

    fix UI/UX and update profile for admin

--- front-end/src/components/Sidebar/SidebarHead.jsx
+++ front-end/src/components/Sidebar/SidebarHead.jsx
@@ -8,10 +8,13 @@ import RuleFolderOutlinedIcon from '@mui/icons-material/RuleFolderOutlined';
 import { getTokenFromUrlAndSaveToStorage } from '../tokenutils';
 import axiosInstance from '../../API/axios';
 import './SidebarHead.scss'
+import { useNavigate } from 'react-router-dom'; 
 
 function SidebarHead() {
     const [expand, setExpand] = useState(true);
     const [isAvatarVisible, setIsAvatarVisible] = useState(false);
+    const [activeItem, setActiveItem] = useState('');
+    const navigate = useNavigate(); 
 
     const toggleSidebar = () => {
         setExpand(!expand);
@@ -20,6 +23,10 @@ function SidebarHead() {
 
     const [head, setHead] = useState({});
 
+    const handleItemClick = (item) => {
+        setActiveItem(item);
+        navigate(`/${item}`);
+    };
 
     useEffect(() => {
         const userToken = getTokenFromUrlAndSaveToStorage();
@@ -71,42 +78,42 @@ function SidebarHead() {
                     )}
                     <hr style={{ color: '#fff' }}></hr>
                     <ul className="sidebar-nav">
-                        <li className="sidebar-item">
-                            <a href="/homeHead" className="sidebar-link">
+                        <li className={`sidebar-item ${activeItem === 'homeHead' ? 'active' : ''}`}>
+                            <a href="#" className="sidebar-link" onClick={() => handleItemClick('homeHead')}>
                                 <i className="lni lni-user"><HomeOutlinedIcon /></i>
                                 <span>Trang chủ</span>
                             </a>
                         </li>
-                        <li className="sidebar-item">
-                            <a href="/profileHead" className="sidebar-link">
+                        <li className={`sidebar-item ${activeItem === 'profileHead' ? 'active' : ''}`}>
+                            <a href="#" className="sidebar-link" onClick={() => handleItemClick('profileHead')}>
                                 <i className="lni lni-agenda"><PersonOutlinedIcon /></i>
                                 <span>Trang cá nhân</span>
                             </a>
                         </li>
-                        <li className="sidebar-item">
-                            <a href="/registerHead" className="sidebar-link">
+                        <li className={`sidebar-item ${activeItem === 'registerHead' ? 'active' : ''}`}>
+                            <a href="#" className="sidebar-link" onClick={() => handleItemClick('registerHead')}>
                                 <i className="lni lni-popup"><RecentActorsOutlinedIcon /></i>
                                 <span>Đăng ký đề tài</span>
                             </a>
                         </li>
-                        <li className="sidebar-item">
+                        <li className={`sidebar-item ${activeItem === 'management' ? 'active' : ''}`}>
                             <a href="#" className="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse" data-bs-target="#auth"
                                 aria-expanded="false" aria-controls="auth">
                                 <i className="lni lni-protection"><FactCheckOutlinedIcon/></i>
                                 <span>Quản lý đề tài</span>
                             </a>
                             <ul id="auth" className="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                                <li className="sidebar-item">
-                                    <a  style={{marginLeft:'10px'}} href="/managermentHead/approve" className="sidebar-link">Duyệt đề tài</a>
+                                <li className={`sidebar-item ${activeItem === 'managermentHead/approve' ? 'active' : ''}`}>
+                                    <a  style={{marginLeft:'10px'}} href="#" className="sidebar-link" onClick={() => handleItemClick('managermentHead/approve')}>Duyệt đề tài</a>
                                 </li>
-                                <li className="sidebar-item">
-                                    <a style={{marginLeft:'10px'}} href="/managementHead/assign" className="sidebar-link">Phân giảng viên phản biện</a>
+                                <li className={`sidebar-item ${activeItem === 'managementHead/assign' ? 'active' : ''}`}>
+                                    <a style={{marginLeft:'10px'}} href="#" className="sidebar-link" onClick={() => handleItemClick('managementHead/assign')}>Phân giảng viên phản biện</a>
                                 </li>
-                                <li className="sidebar-item">
-                                    <a style={{marginLeft:'10px'}} href="/managementHead/topics" className="sidebar-link">Đề tài của tôi</a>
+                                <li className={`sidebar-item ${activeItem === 'managementHead/topics' ? 'active' : ''}`}>
+                                    <a style={{marginLeft:'10px'}} href="#" className="sidebar-link" onClick={() => handleItemClick('managementHead/topics')}>Đề tài của tôi</a>
                                 </li>
-                                <li className="sidebar-item">
-                                    <a style={{marginLeft:'10px'}} href="/managermentHead/TopicsPB" className="sidebar-link">Đề tài phản biện</a>
+                                <li className={`sidebar-item ${activeItem === 'managermentHead/TopicsPB' ? 'active' : ''}`}>
+                                    <a style={{marginLeft:'10px'}} href="#" className="sidebar-link" onClick={() => handleItemClick('managermentHead/TopicsPB')}>Đề tài phản biện</a>
                                 </li>
                             </ul>
                         </li>
