12efbd64d9b2a44d8aacbd7768e9449c68a3d8b9
Nguyen <nna9220@gmail.com>
2024-04-28 14:50:47 +0700

    fix edit user of admin

--- back-end/src/main/java/com/web/config/CheckRole.java
+++ back-end/src/main/java/com/web/config/CheckRole.java
@@ -16,12 +16,12 @@ public class CheckRole {
         String token = (String) session.getAttribute("token");
         Claims claims = JwtUtils.extractClaims(token, "f2f1035db6a255e7885838b020f370d702d4bb0f35a368f06ded1ce8e6684a27");
         UserDetails email = userUtils.loadUserByUsername(claims.getSubject());
-        return personRepository.findUserByEmail(email.getUsername());
+        return personRepository.findUsername(email.getUsername());
     }
 
     public static Person getRoleCurrent2(String token, UserUtils userUtils, PersonRepository personRepository){
         Claims claims = JwtUtils.extractClaims(token, "f2f1035db6a255e7885838b020f370d702d4bb0f35a368f06ded1ce8e6684a27");
         UserDetails email = userUtils.loadUserByUsername(claims.getSubject());
-        return personRepository.findUserByEmail(email.getUsername());
+        return personRepository.findUsername(email.getUsername());
     }
 }
